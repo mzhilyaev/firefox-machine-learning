e49f511f4277e58da32c8a3c43b7f13c20bb9c14
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-10-22 16:48:09 -0700

    Refactored tests and test document to run on a collection of docs

--- extension/test/test-LWCA.js
+++ extension/test/test-LWCA.js
@@ -5,51 +5,68 @@ const test = require("sdk/test");
 
 const {LWCAClassifier} = require("lwca_refined");
 let scriptLoader = Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Ci.mozIJSSubScriptLoader);
-scriptLoader.loadSubScript(data.url("test/politics.js"));
-scriptLoader.loadSubScript(data.url("test/health.js"));
-scriptLoader.loadSubScript(data.url("test/investing.js"));
-scriptLoader.loadSubScript(data.url("test/technology.js"));
-scriptLoader.loadSubScript(data.url("test/justice.js"));
-scriptLoader.loadSubScript(data.url("test/business.js"));
-scriptLoader.loadSubScript(data.url("test/sport.js"));
+scriptLoader.loadSubScript(data.url("test/cnn_docs.js"));
 
-let testCollection = [
- {
-  name:  "cnn:politics",
-  expectedCat: "politics",
-  visits: politics,
- },
- {
-  name:  "cnn:investing",
-  expectedCat: "personal finance",
-  visits: investing,
- },
- {
-  name:  "cnn:health",
-  expectedCat: "health & fitness",
-  visits: health,
- },
- {
-  name:  "cnn:technology",
-  expectedCat: "technology & computing",
-  visits: technology,
- },
- {
-  name:  "cnn:justice",
-  expectedCat: "law",
-  visits: justice,
- },
- {
-  name:  "cnn:sport",
-  expectedCat: "sport",
-  visits: justice,
- },
- {
-  name:  "cnn:business",
-  expectedCat: "business",
-  visits: justice,
- },
-];
+function testVisits(visits) {
+   let titleResults = {};
+   let fullTextResults = {};
+   let fullTextCount = 0;
+   let len = visits.length;
+   let ftLen = 20;
+   for (let visit of visits) {
+      category = c.classify(visit[0], visit[1]);
+      titleResults[category] = (titleResults[category] || 0) + 1;
+      if (fullTextCount < ftLen) {
+        category = c.classify(visit[0], visit[1] + " " + visit[2]);
+        fullTextResults[category] = (fullTextResults[category] || 0) + 1;
+        fullTextCount++;
+      }
+    }
+    Object.keys(titleResults).forEach(cat => {
+      titleResults[cat] = Math.round(titleResults[cat] * 100 / len);
+    });
+    Object.keys(fullTextResults).forEach(cat => {
+      fullTextResults[cat] = Math.round(fullTextResults[cat] * 100 / ftLen);
+    });
+    return {title: titleResults, fullText: fullTextResults};
+}
+
+function outputCatResults(results, catName, expectedCat) {
+  dump("  set category: " + catName);
+  if (expectedCat) {
+     dump(", mapped to: '" + expectedCat + "', title precision: " +
+          (results.title[expectedCat] || 0) + "%" +
+          ", fulltext precision: " +
+          (results.fullText[expectedCat] || 0) + "%");
+  }
+  dump("\n");
+  let titleOrdered = Object.keys(results.title).sort((a,b) => {return results.title[b] - results.title[a];});
+  let fullTextOrdered = Object.keys(results.fullText).sort((a,b) => {return results.fullText[b] - results.fullText[a];});
+  dump ("\t  Url+Title\t\t\t  Url+FullText\n");
+  while (titleOrdered.length) {
+    let tCat = titleOrdered.shift();
+    let fCat = fullTextOrdered.shift();
+    dump("\t" + tCat + ":" + results.title[tCat] + "%");
+    if (fCat) {
+      dump("\t\t\t" + fCat + ":" + results.fullText[fCat] + "%");
+    }
+    dump("\n");
+  }
+  dump("\n");
+}
+
+function procTestSet(testSet, name) {
+  dump("TEST SET: " + name + "\n");
+  Object.keys(testSet).forEach(cat => {
+    let obj = testSet[cat];
+    if (obj instanceof Array) {
+    }
+    else {
+      let results = testVisits(obj.visits);
+      outputCatResults(results, obj.name, obj.expectedCat);
+    }
+  });
+}
 
 
 exports["test lwca"] = function test_PrefsManagerPrefs(assert, done) {
@@ -59,25 +76,13 @@ exports["test lwca"] = function test_PrefsManagerPrefs(assert, done) {
       c = new LWCAClassifier();
       yield c.init();
       dump("Fnished loading LWCA\n");
+      /*
       testCollection.forEach(collection => {
-        let results = {};
-        results[collection.expectedCat] = 0;
-        // collect categorization results
-        for (let visit of collection.visits) {
-          category = c.classify(visit[0], visit[1]);
-          results[category] = (results[category] || 0) + 1;
-        }
-        let len = collection.visits.length;
-        let precision = Math.round((results[collection.expectedCat] || 0) * 100/ len);
-        dump("\n\nTest set: " + collection.name +
-             ", Expected: '" + collection.expectedCat +
-             "', Precision: " + precision +
-             "%\n");
-        let ordered = Object.keys(results).sort((a,b) => {return results[b] - results[a];});
-        ordered.forEach(category => {
-          dump("    " + category + ":" + Math.round(results[category] * 100 / len) + "%\n");
-        });
+        let results = testVisits(collection.visits);
+        outputCatResults(results, collection.name, collection.expectedCat);
       });
+      */
+      procTestSet(cnnTest, "CNN PATH SELECTED DOCS");
       assert.ok(true);
       done();
     } catch (ex) {
